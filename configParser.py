89fa7f3b7b520827a088b1f6cb708e33f71c5968
corekms <corekms@gmail.com>
2018-12-28 06:43:46 +0000

    ..

---
+++ configParser.py
@@ -0,0 +1,10 @@
+''' configFile.txt format
+[DatabaseSection]
+database.dbname=unitTest
+database.user=root
+database.password=**** '''
+
+import ConfigParser
+config = ConfigParser.RawConfigParser()
+config.read('ConfigFile.properties')
+print config.get('DatabaseSection', 'database.dbname');
